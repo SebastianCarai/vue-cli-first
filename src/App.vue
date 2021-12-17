6fa2863cd855e3418a0c768afdd9bb75d69f77b0
SebastianCarai <sebastiancarai7@gmail.com>
2021-12-17 15:00:53 +0100

    done

--- src/App.vue
+++ src/App.vue
@@ -1,17 +1,23 @@
 <template>
   <div id="app">
-    <img alt="Vue logo" src="./assets/logo.png" />
+    <Title />
+    <Jumbotron />
+    <img alt="Vue logo" src="./assets/img/logo.png" />
     <HelloWorld msg="Welcome to Your Vue.js App" />
   </div>
 </template>
 
 <script>
 import HelloWorld from "./components/HelloWorld.vue";
+import Title from "./components/Title.vue";
+import Jumbotron from "./components/Jumbotron.vue"
 
 export default {
   name: "App",
   components: {
     HelloWorld,
+    Title,
+    Jumbotron
   },
 };
 </script>
---
+++ src/components/Jumbotron.vue
@@ -0,0 +1,25 @@
+<template>
+    <div>
+        <h2>Sono un jumbotron</h2>
+    </div>
+</template>
+
+<script>
+export default{
+    name:"Jumbotron"
+}
+</script>
+
+<style scoped>
+div{
+    background-image: url('https://cutewallpaper.org/21/jumbotron-background-image/new-jumbotron-background-Gold-Bond-Ultimate.png');
+    color: white;
+    height: 500px;
+    display: flex;
+    align-items: center;
+    justify-content: center;
+}
+div h2{
+    font-size: 50px;
+}
+</style>
